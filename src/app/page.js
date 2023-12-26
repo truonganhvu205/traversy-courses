834cba2111306325164abf3a6d89cfd54caa2a4a
vu <truonganhvu205@gmail.com>
2023-12-26 18:05:51 +0700

    update config

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Loading from './loading'
 import Courses from './components/Courses'
 import CourseSearch from './components/CourseSearch'
